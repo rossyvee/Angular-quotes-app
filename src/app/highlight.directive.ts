e2fe16a7e688a39f2dd1186dd5ee95fb486936a4
rossyvee <roseakinyi001@gmail.com>
2022-04-09 23:59:35 +0300

    adding background colour annd border radius to the file

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,6 +5,8 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) { }
-
+  constructor(private elem:ElementRef) {
+    elem.nativeElement.style.backgroundColor = 'pink'
+    elem.nativeElement.style.borderRadius = '15px'
+   }
 }
