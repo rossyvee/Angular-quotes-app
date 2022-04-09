edacf043a377c87c694cdd28956f1a440b00fc84
rossyvee <roseakinyi001@gmail.com>
2022-04-09 15:07:35 +0300

    pointing call back to the constructor element

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -13,7 +13,9 @@ quotes: Quote[]=[
   new Quote(2, 'Peter','Revelation','Astrology reveals the will of the gods','Juvenal',new Date(2022,4,10),0,0),
   new Quote(3, 'Maureen','Tales','Astrology is one of the oldest and most accurate tools known to mankind','Chris Flisher',new Date(2022,4,11),0,0),
   new Quote(4, 'Bruno','Physician','Astrology is a Language. If you understand this language, The Sky Speaks to You','Dane Rudhyar',new Date(2022,4,13),0,0),
-  new Quote(6, 'James','Weather Report','Astrology is like a weather report; it tells you what conditions you are likely to face in the future','LEE GOLDBERG',new Date(2022,4,14),0,0),
+  new Quote(5, 'James','Weather Report','Astrology is like a weather report, it tells you what conditions you are likely to face in the future','Lee Goldberg',new Date(2022,4,14),0,0),
+  new Quote(6, 'Nolan',' Report','Astrology is a disease, not a science','Lee Goldberg',new Date(2022,4,14),0,0),
+
 ]
 
   constructor() { }
