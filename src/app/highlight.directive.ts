793dd1cae69e4161eb8a146364ad18651bc7850c
rossyvee <roseakinyi001@gmail.com>
2022-04-10 23:15:38 +0300

    adding style to the borderRadius in the workspace

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -8,5 +8,6 @@ export class HighlightDirective {
   constructor(private elem:ElementRef) {
     elem.nativeElement.style.backgroundColor = 'pink'
     elem.nativeElement.style.borderRadius = '15px'
+    elem.nativeElement.style.padding = '20px'
    }
 }
